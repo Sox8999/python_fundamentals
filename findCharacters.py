71fcc31ac43dae10dacc7d3d176cc55f5e816631
Sox8999 <bwroeben@gmail.com>
2017-08-09 02:52:05 -0400

    Find character in string

---
+++ findCharacters.py
@@ -0,0 +1,27 @@
+'''Assignment: Find Characters
+Write a program that takes a list of strings and a string containing a single character, and prints a new list of all the strings containing that character.
+
+Here's an example:
+
+# input
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+# output
+new_list = ['hello','world']
+Copy
+Hint: how many loops will you need to complete this task?
+'''
+
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+
+def find_char(lst):
+    new_lst = []
+
+    for text in lst:
+        if char in text:
+            new_lst += text
+    print new_lst
+
+
+find_char(word_list)
