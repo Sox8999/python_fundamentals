ee9be11ac2a180f7b8b5fddc066d79e47f3664f6
Sox8999 <bwroeben@gmail.com>
2017-10-23 12:32:04 -0700

    Students and Instructors dictionary

---
+++ names.py
@@ -0,0 +1,86 @@
+"""
+Assignment: Names
+Write the following function.
+
+Part I
+Given the following list:
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+Copy
+Create a program that outputs:
+
+Michael Jordan
+John Rosales
+Mark Guillen
+KB Tonel
+Copy
+Part II
+Now, given the following dictionary:
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+Copy
+Create a program that prints the following format (including number of characters in each combined name):
+
+Students
+1 - MICHAEL JORDAN - 13
+2 - JOHN ROSALES - 11
+3 - MARK GUILLEN - 11
+4 - KB TONEL - 7
+Instructors
+1 - MICHAEL CHOI - 11
+2 - MARTIN PURYEAR - 13
+"""
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+def show_students(arr):
+    for student in students:
+        print student['first_name'], student['last_name']
+
+def show_all(users):
+    for role in users:
+        counter = 0
+        print role
+        for person in users[role]:
+            counter += 1
+            first_name = person['first_name'].upper()
+            last_name = person['last_name'].upper()
+            length = len(first_name) + len(last_name)
+            print "{} - {} {} - {}".format(counter, first_name, last_name, length)
+
+show_students(students)
+show_all(users)
